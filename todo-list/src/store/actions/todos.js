02abb5005ac2f575eda7a1ed70790bde52a6c39d
pambatista <pambatista93@gmail.com>
2019-12-10 21:18:35 -0300

    create action completedTodo

--- todo-list/src/store/actions/todos.js
+++ todo-list/src/store/actions/todos.js
@@ -1,3 +1,5 @@
 export const addTodo = (text) => ({ type: 'ADD_TODO', playload: { text } });
 
 export const removeTodo = (id) => ({ type: 'REMOVE_TODO', playload: { id } });
+
+export const completedTodo = (todo) => ({ type: 'COMPLETED_TASK' , playload: { id: todo.id, text: todo.text, completed: !todo.completed } });
--- todo-list/src/store/reducers/todos.js
+++ todo-list/src/store/reducers/todos.js
@@ -1,20 +1,24 @@
 const INITIAL_STATE = [
   {
     id: 1,
-    text: "Estudar React",
+    text: 'Estudar React',
+    completed: false
   },
   {
     id: 2,
-    text: "Tomar café",
-  }
+    text: 'Tomar café',
+    completed: false
+  },
 ];
 
 export default function todos(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'ADD_TODO':
-      return [...state, { id: Math.random(), text: action.playload.text } ];
+      return [...state, { id: Math.random(), text: action.playload.text, completed: false }];
     case 'REMOVE_TODO':
-      return state.filter(todo => todo.id !== action.playload.id);
+      return state.filter((todo) => todo.id !== action.playload.id);
+    case 'COMPLETED_TASK':
+      return state.map((todo) => todo.id === action.playload.id ? action.playload : todo );
     default:
       return state;
   }
